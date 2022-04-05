9718efc11bfb7a7a97b08287ff74cc2e3363526f
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2022-04-05 10:34:42 +0200

    Store queries text rather than their tokens + Some minor refactorings

--- create_sbert_datasets.py
+++ create_sbert_datasets.py
@@ -6,52 +6,53 @@ from query_pattern_regex import *
 from query_pattern import QueryPatterns, QueryPattern
 
 
-def create_siamese_row(query_tokens_one: list, query_tokens_two: list, similariry_score: int):
+def create_siamese_row(query_one: str, query_two: str, similarity_score: int):
     return pd.Series({
-        Constants.COLUMN_NAME_QUERY_ONE: query_tokens_one,
-        Constants.COLUMN_NAME_QUERY_TWO: query_tokens_two,
-        Constants.COLUMN_NAME_SIMILARITY_SCORE: similariry_score
+        Constants.COLUMN_NAME_QUERY_ONE: query_one,
+        Constants.COLUMN_NAME_QUERY_TWO: query_two,
+        Constants.COLUMN_NAME_SIMILARITY_SCORE: similarity_score
     })
 
 
-def create_siamese_batch_row(query_tokens_base: list, query_tokens_two_list: list, similarity_score: int):
-    output = pd.DataFrame(columns=[Constants.COLUMN_NAME_QUERY_ONE, Constants.COLUMN_NAME_QUERY_TWO,
-                                   Constants.COLUMN_NAME_SIMILARITY_SCORE])
-    for query_two in query_tokens_two_list:
-        output = output.append({
-            Constants.COLUMN_NAME_QUERY_ONE: query_tokens_base,
-            Constants.COLUMN_NAME_QUERY_TWO: query_two,
-            Constants.COLUMN_NAME_SIMILARITY_SCORE: similarity_score
-        }, ignore_index=True)
+def create_siamese_batch_row(query_base: str, query_two_series: pd.Series, similarity_score: int):
+    output = pd.DataFrame(
+        columns=[
+            Constants.COLUMN_NAME_QUERY_ONE,
+            Constants.COLUMN_NAME_QUERY_TWO,
+            Constants.COLUMN_NAME_SIMILARITY_SCORE
+        ])
+
+    # Be aware that, here, the order of lines matters!
+    output[Constants.COLUMN_NAME_QUERY_TWO] = query_two_series
+    output[Constants.COLUMN_NAME_QUERY_ONE] = query_base
+    output[Constants.COLUMN_NAME_SIMILARITY_SCORE] = similarity_score
+
     return output
 
 
-def create_triplet_row(query_tokens_base: list, query_tokens_negative: list, query_tokens_positive: int):
+def create_triplet_row(query_base: str, query_negative: str, query_positive: str):
     return pd.Series({
-        Constants.COLUMN_NAME_ANCHOR: query_tokens_base,
-        Constants.COLUMN_NAME_POSITIVE: query_tokens_positive,
-        Constants.COLUMN_NAME_NEGATIVE: query_tokens_negative
+        Constants.COLUMN_NAME_ANCHOR: query_base,
+        Constants.COLUMN_NAME_POSITIVE: query_positive,
+        Constants.COLUMN_NAME_NEGATIVE: query_negative
     })
 
 
 def create_triplet_batch_row(
-        query_tokens_base: list,
-        query_tokens_negative_list: pd.Series,
-        query_tokens_positive_list: pd.Series
+        query_base: list,
+        query_negative_series: pd.Series,
+        query_positive_series: pd.Series
 ):
     output = pd.DataFrame(columns=[
         Constants.COLUMN_NAME_ANCHOR,
         Constants.COLUMN_NAME_POSITIVE,
         Constants.COLUMN_NAME_NEGATIVE
     ])
-    rows_count = min(len(query_tokens_negative_list), len(query_tokens_positive_list))
 
-    for i in range(rows_count):
-        output = output.append({
-            Constants.COLUMN_NAME_ANCHOR: query_tokens_base,
-            Constants.COLUMN_NAME_POSITIVE: query_tokens_positive_list.iloc[i],
-            Constants.COLUMN_NAME_NEGATIVE: query_tokens_negative_list.iloc[i]
-        }, ignore_index=True)
+    # Be aware that, here, the order of lines matters!
+    output[Constants.COLUMN_NAME_POSITIVE] = query_positive_series
+    output[Constants.COLUMN_NAME_NEGATIVE] = query_negative_series
+    output[Constants.COLUMN_NAME_ANCHOR] = query_base
 
     return output
 
@@ -72,16 +73,16 @@ def find_similar_queries(df: pd.DataFrame, query: str, label: int, similar_label
         random_selection = list(
             np.random.randint(low=0, high=len(all_similar_queries), size=Constants.SIMILAR_QUERIES_COUNT))
 
-    similar_queries = all_similar_queries.iloc[random_selection]['tokens']
-    return similar_queries
+    similar_queries = all_similar_queries.iloc[random_selection]['full_query']
+    return similar_queries.reset_index(drop=True)
 
 
 def find_different_queries(df: pd.DataFrame, label: int, similar_labels: list):
-    different_df = df[(df.labels != label) & (~df.labels.isin(similar_labels))]['tokens']
+    different_df = df[(df.labels != label) & (~df.labels.isin(similar_labels))]['full_query']
     random_selection = list(np.random.randint(low=0, high=len(different_df), size=Constants.DIFFERENT_QUERIES_COUNT))
 
     different_queries = different_df.iloc[random_selection]
-    return different_queries
+    return different_queries.reset_index(drop=True)
 
 
 if __name__ == '__main__':
@@ -119,10 +120,15 @@ if __name__ == '__main__':
         similar_queries = find_similar_queries(labeled_df, query, label, similar_labels)
         different_queries = find_different_queries(labeled_df, query, similar_labels)
 
-        siamese_df = siamese_df.append(create_siamese_batch_row(tokens, similar_queries, 1))
-        siamese_df = siamese_df.append(create_siamese_batch_row(tokens, different_queries, 0))
+        siamese_df = siamese_df.append(create_siamese_batch_row(query, similar_queries, 1))
+        siamese_df = siamese_df.append(create_siamese_batch_row(query, different_queries, 0))
 
-        triplet_df = triplet_df.append(create_triplet_batch_row(tokens, different_queries, similar_queries))
+        triplet_df = triplet_df.append(create_triplet_batch_row(query, different_queries, similar_queries))
+
+    assert not siamese_df.isnull().values.any()
+    assert not triplet_df.isnull().values.any()
 
     siamese_df.to_csv(Constants.DATASET_PATH_SIAMESE, index=False)
     triplet_df.to_csv(Constants.DATASET_PATH_TRIPLET, index=False)
+
+    print('Datasets are saved!')
